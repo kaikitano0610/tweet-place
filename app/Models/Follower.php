b00c14144c8ae4a764ee4e79d8f572570da96276
kaikitano0610 <k.kitano.kai@gmail.com>
2024-10-18 16:42:29 +0900

    型の追加

--- app/Models/Follower.php
+++ app/Models/Follower.php
@@ -20,11 +20,11 @@ class Follower extends Model
     public $timestamps = false;
     public $incrementing = false;
 
-    public function getFollowCount($user_id)
+    public function getFollowCount(int $user_id)
     {
         return $this->where('following_id',$user_id)->count();
     }
-    public function getFollowerCount($user_id)
+    public function getFollowerCount(int $user_id)
     {
         return $this->where('followed_id',$user_id)->count();
     }
