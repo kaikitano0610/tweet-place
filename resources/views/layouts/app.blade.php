6593cece627115a8d7f94fb629a39003704bc031
kaikitano0610 <k.kitano.kai@gmail.com>
2024-10-18 16:05:38 +0900

    ユーザー詳細画面の作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,16 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('users',UsersController::class,['only' => ['index','show','edit','update']]);
+    // 下記が含まれる
+    // ユーザー一覧ページ（GET /users）
+    // Route::get('/users', [UsersController::class, 'index'])->name('users.index');
+    // ユーザー詳細ページ（GET /users/{user}）
+    // Route::get('/users/{user}', [UsersController::class, 'show'])->name('users.show');
+    // ユーザー編集ページ（GET /users/{user}/edit）
+    // Route::get('/users/{user}/edit', [UsersController::class, 'edit'])->name('users.edit');
+    // ユーザー更新処理（PUT /users/{user}）
+    // Route::put('/users/{user}', [UsersController::class, 'update'])->name('users.update');
+
     #フォロー、フォロー解除
     Route::post('users/{user}/follow',[UsersController::class,'follow'])->name('follow');
     Route::delete('users/{user}/unfollow',[UsersController::class,'unfollow'])->name('unfollow');
